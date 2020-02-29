c53efcad1e1513fd1c13e06765bb300282f33fd6
usuario github <correo>
2020-02-28 20:11:25 -0600

    pagos

---
+++ aplicacionweb/src/components/Pagos.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ListPagos from './ListPagos';
+
+
+
+class Pagos extends Component{
+
+
+    
+render(){
+    
+    return(
+        
+        <div>
+
+            <ListPagos ></ListPagos>
+        </div>
+        
+    )
+}
+}
+
+export default Pagos
