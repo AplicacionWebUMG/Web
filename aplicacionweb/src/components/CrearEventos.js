dcaa038ada4feedc7a070896625bf04fde926e13
Josedaniel98 <josevego98@gmail.com>
2020-03-26 21:34:57 -0600

    Crear evento

--- aplicacionweb/src/components/CrearEventos.js
+++ aplicacionweb/src/components/CrearEventos.js
@@ -6,92 +6,94 @@ class CrearEventos extends Component{
 
             this.state = {
                 nombre: "",
-                fecha: "",
-                hora: "",
+                // fecha: "",
+                // hora: "",
                 lugar: "",
-                tipo: 0,
-                asientos: "",
-                estado: 0,
+                // tipo: 0,
+                // asientos: "",
+                // estado: 0,
                 organizadores: "",
-                costo: 0
+                // costo: 0
     }
 }
 render(){
     return(
         <div>
-            <form onSubmit={this.onSubmit.bind(this)}>
+           
 
-            <div class="container-fluid mt--6">
-                <div class="row">
-                    <div class="col">
-                         <div class="card">
+            <div className="container-fluid mt--6">
+                <div className="row">
+                    <div className="col">
+                         <div className="card">
             
-                             <div class="table-responsive">
-                                 <div class="card-body">   
-                                    <form>
-                                        <h6 class="heading-small text-muted mb-4">Informacion del evento</h6>
+                             <div className="table-responsive">
+                                 <div className="card-body">   
+                                 <form onSubmit={this.onSubmit.bind(this)}>
+                                        <h6 className="heading-small text-muted mb-4">Informacion del evento</h6>
                                         
-                                            <div class="pl-lg-4">
-                                                <div class="row">
-                                                    <div class="col-lg-6">
-                                                        <div class="form-group">
-                                                            <label class="form-control-label" for="nombre-evento">Nombre del evento</label>
-                                                            <input type="text" id="nombre" class="form-control" placeholder="Nombre del evento" value={this.state.nombre} onChange={this.NombreInputChange.bind(this)} />  
+                                            <div className="pl-lg-4">
+                                                <div className="row">
+                                                    <div className="col-lg-6">
+                                                        <div className="form-group">
+                                                            <label className="form-control-label" htmlFor="nombre-evento">Nombre del evento</label>
+                                                            <input type="text" id="nombre" className="form-control" placeholder="Nombre del evento" value={this.state.nombre} onChange={this.NombreInputChange.bind(this)} />  
 
                                                         </div>
-                                                        {/* <div class="col-lg-6">
-                                                            <div class="form-group">
-                                                                <label class="form-control-label" for="input-fecha">fecha</label>
-                                                                <input type="datetime-local" id="fecha" class="form-control"  value={this.state.fecha} onChange={this.FechaInputChange.bind(this)}/>
+                                                        {/* <div className="col-lg-6">
+                                                            <div className="form-group">
+                                                                <label className="form-control-label" for="input-fecha">fecha</label>
+                                                                <input type="datetime-local" id="fecha" className="form-control"  value={this.state.fecha} onChange={this.FechaInputChange.bind(this)}/>
                                                             </div>
                                                         </div> */}
-                                                        <div class="col-lg-4">
-                                                            <div class="form-group">
-                                                                <label class="form-control-label" for="input-lugar">Lugar</label>
-                                                                <input type="text" id="lugar" class="form-control" placeholder="Cayala-Ciudad de Guatemala" value={this.state.lugar} onChange={this.LugarInputChange.bind(this)}/>
+                                                        <div className="col-lg-4">
+                                                            <div className="form-group">
+                                                                <label className="form-control-label" htmlFor="input-lugar">Lugar</label>
+                                                                <input type="text" id="lugar" className="form-control" placeholder="Cayala-Ciudad de Guatemala" value={this.state.lugar} onChange={this.LugarInputChange.bind(this)}/>
                                                             </div>
                                                         </div>
 
-                                                        {/* <div class="col-lg-4">
-                                                            <div class="form-group">
-                                                                <label class="form-control-label" for="input-date">Estado del evento</label>
-                                                                <select className="form-control" id="estado" value={this.state.estado} onChange={this.EstadoInputChange.bind(this)} >
+                                                        {/* <div className="col-lg-4">
+                                                            <div className="form-group">
+                                                                <label className="form-control-label" for="input-date">Estado del evento</label>
+                                                                <select classNameName="form-control" id="estado" value={this.state.estado} onChange={this.EstadoInputChange.bind(this)} >
                                                                     <option value={1} >asiento libre</option>
                                                                     <option value={3} >asiento ocupado</option>
                                                                 </select>
                                                             </div>
                                                          </div>     */}
+                                                         
+                                                    <div className="col-md-12">
+                                                        <div className="form-group">
+                                                            <label className="form-control-label" htmlFor="input-organizadores">Organizadores</label>
+                                                            <input id="organizadores" className="form-control" placeholder="Promusa Eventos" type="text" value={this.state.organizadores} onChange={this.OrganizadoresInputChange.bind(this)}/>
+                                                        </div>
+                                                    </div>
+                                               
+                                               
+                                                
                                                     </div>
                                                 </div>
+                                               
                                             </div>
-                                            <hr class="my-4" />
-                                            <h6 class="heading-small text-muted mb-4">Localidadees</h6>
-                                            <div class="row">
-                                                {/* <div class="col-lg-4">
-                                                    <div class="form-group">
-                                                        <label class="form-control-label" for="input-tipo">Tipo</label>
-                                                        <select className="form-control" id="tipo" value={this.state.tipo} onChange={this.TipoInputChange.bind(this)} >
+                                           
+                                                {/* <div className="col-lg-4">
+                                                    <div className="form-group">
+                                                        <label className="form-control-label" for="input-tipo">Tipo</label>
+                                                        <select classNameName="form-control" id="tipo" value={this.state.tipo} onChange={this.TipoInputChange.bind(this)} >
                                                             <option value={1} >1</option>
                                                             <option value={3} >3</option>
                                                             <option value={5} >5</option>
                                                         </select>
                                                     </div>
-                                                </div> */}
-                                                {/* <div class="col-lg-4">
-                                                    <div class="form-group">
-                                                        <label class="form-control-label" for="input-costo">Costo</label>
-                                                        <input type="number" id="costo" class="form-control" placeholder="Q."  value={this.state.costo} onChange={this.CostoInputChange.bind(this)}/>
-                                                    </div>
-                                                </div> */}
-                                                <div class="row">
-                                                    <div class="col-md-12">
-                                                        <div class="form-group">
-                                                            <label class="form-control-label" for="input-organizadores">Organizadores</label>
-                                                            <input id="organizadores" class="form-control" placeholder="Promusa Eventos" type="text" value={this.state.Organizadores} onChange={this.OrganizadoresInputChange.bind(this)}/>
-                                                        </div>
-                                                    </div>
                                                 </div>
-                                            </div>
+                                                <div className="col-lg-4">
+                                                    <div className="form-group">
+                                                        <label className="form-control-label" for="input-costo">Costo</label>
+                                                        <input type="number" id="costo" className="form-control" placeholder="Q."  value={this.state.costo} onChange={this.CostoInputChange.bind(this)}/>
+                                                    </div>
+                                                </div> */}
+                                                
+                                                <button type="submit" className="btn btn-primary">Enviar</button>  
                                     </form>
                   
                                  </div>
@@ -101,11 +103,11 @@ render(){
                     </div>
                 </div>
 
-                <footer class="footer pt-0">
+                <footer className="footer pt-0">
                 </footer>
             </div>
-            <button type="submit" className="btn btn-primary">Enviar</button>
-            </form>
+            
+          
         </div>
     )
 }
