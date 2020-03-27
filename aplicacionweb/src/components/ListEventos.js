abc0706ac8d3bf004b6c020b9bc9ca275f6857ee
Madelin <madymoffu2697@gmail.com>
2020-03-27 13:25:45 -0600

    Listar Eventos

--- aplicacionweb/src/components/ListEventos.js
+++ aplicacionweb/src/components/ListEventos.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'jquery';
+import 'popper.js';
+import '../../node_modules/bootstrap/dist/js/bootstrap';
 
 
 class ListEventos extends Component{
@@ -8,43 +12,93 @@ class ListEventos extends Component{
         this.state={eventos:[]}
     }
     componentDidMount(){
-        fetch('./ficheros/Eventos.json')
+        fetch('https://rijhn09.pythonanywhere.com/evento/mostrar/', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json' 
+            }
+        
+    })
         .then(response=>{return response.json()})
-        .then(eventos =>{this.setState({eventos: eventos.eventos})})
+        .then(eventos =>{this.setState({eventos: eventos})})
          
-    }
+    
+}
     render(){
         return(
             <div>
-                <h1> Eventos</h1>
-                 {
+                {/* titulo */}
+                 <div class="header bg-primary pb-6">
+      <div class="container-fluid">
+        <div class="header-body">
+          <div class="row align-items-center py-4">
+            <div class="col-lg-6 col-7">
+              <h6 class="h2 text-white d-inline-block mb-0">Eventos Disponibles</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* fin del titulo */}
+                
+                {
                      this.state.eventos.map(events =>{
                         return(
-                            <div>
-                                    <table>
-                                        <tbody>
-                                            <tr>
-                                                <th> {events.id}. </th>
-                                                <th> {events.nombre} </th>
-                                                <th> {events.afiche} </th>
-                                                <th> {events.fecha} </th>
-                                                <th> {events.hora} </th>
-                                                <th> {events.lugar} </th>
-                                                <th> {events.disponible} </th>
-                                               
+                            <div class="container-fluid mt--6">
+                                <div class="row">
+                                    <div class="col">
+                                    <div class="card">
+                                        <div class="table-responsive">
+                                        <div class="card-body">
+                                            <table class="table align-items-center table-flush">
+                                            <thead class="thead-light">
+                                                <tr>
+                                                <th scope="col">Evento</th>
+                                                <th scope="col">Organizadores</th>
+                                                <th scope="col">Fecha del evento</th>
+                                                <th scope="col">Lugar del Evento</th>
+                                                <th scope="col">Acciones</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody> {/* mostrar datos  */}
+                                                <tr>
+                                                <th scope="row">
+                                                {events.nombre}
+                                                  
 
-
-                                              
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                        )
-                    })
-                 }
+                                                </th> 
+                                                <td> {events.organizadores}</td>
+                                                <td> {events.fecha}</td>
+                                                <td> {events.lugar}</td>
+                                                <td>
+                                                    <a href="#!" class="btn btn-sm btn-warning">Editar</a>
+                                                    <a href="#!" class="btn btn-sm btn-danger">Eliminar</a>
+                                                </td>
+                                                
+                                                </tr>
+                                
+                                        
+                                            </tbody>
+                                            </table>
+                                        </div>
+                                        </div>
+                                    </div>
+                                        </div>
+                                    
+                                        </div>
+                            </div>
+                                  
+                        
+                    )
+                })}
             </div>
+            
         )
     }
+
+
 }
 
-export default ListEventos
+
+export default ListEventos;
